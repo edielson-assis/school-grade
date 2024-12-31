3ba46a37b5c6cc170cfddf029456386471bdec8a
Edielson Assis <edielsonnice@hotmail.com>
2024-12-31 09:43:44 -0300

    feat: created AuthUser microservice

---
+++ authuser/src/test/java/br/com/edielsonassis/authuser/AuthuserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edielsonassis.authuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
